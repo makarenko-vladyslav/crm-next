7649fed774150ed25e7f242207a708d5498125e9
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-08-24 18:49:09 +0200

    Add dashboard slots, parallel routes

--- src/app/(admin)/companies/[id]/not-found.tsx
+++ src/app/(admin)/companies/[id]/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound({}: NotFoundProps) {
     return (
         <div>
             <p>Could not found company</p>
-            
+
             <Link href="/companies" className="text-blue-500">
                 Back to companies
             </Link>
