2ea81a385f3f6b5d8b8c7e89522f3ce9eceb1606
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-08-25 21:03:40 +0200

    variables conflict

--- src/app/components/company-promotions.tsx
+++ src/app/components/company-promotions.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getPromotions } from "@/lib/api";
-import Promotion from "@/app/components/promotion";
+import Promotions from "@/app/components/promotion";
 
 export interface CompanyPromotionsProps {
     companyId: string;
@@ -20,7 +20,7 @@ export default function CompanyPromotions({ companyId }: CompanyPromotionsProps)
         <div className="grid grid-cols-12 gap-5">
             {data?.map((promotion) => (
                 <div key={promotion.id} className="col-span-4">
-                    <Promotion promotion={promotion} />
+                    <Promotions promotion={promotion} />
                 </div>
             ))}
         </div>
